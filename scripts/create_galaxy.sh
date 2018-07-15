fd2a8205ecd3fd10e4dba91d27644f6cf41b76d2
Oles <olestourko@gmail.com>
2018-07-15 16:57:36 -0400

    Fixed path to internal create_galaxy script

--- scripts/create_galaxy.sh
+++ scripts/create_galaxy.sh
@@ -2,4 +2,4 @@
 GALAXY_NAME=$1
 ADMIN_ID=$2
 # SERVER_PATHNAME and GALAXY_PATH are set in the Dockerfile
-(sleep 10; echo '/save'; sleep 1; '/stop' ) | sh ${SERVER_PATHNAME} --galaxy-name $GALAXY_NAME --admin $ADMIN_ID --datapath $GALAXY_PATH
+(sleep 10; echo '/save'; sleep 1; echo '/stop' ) | sh ${SERVER_PATHNAME} --galaxy-name $GALAXY_NAME --admin $ADMIN_ID --datapath $GALAXY_PATH
